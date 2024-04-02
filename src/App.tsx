60184ebfa2be3e96faa793b5344ea0d2b3d75cf1
GabS. <gabrielsoares1602@gmail.com>
2024-04-02 16:30:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Tittlebar from "./Tittlebar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Tittlebar />
+    </div>
+  );
+}
+
+export default App;
